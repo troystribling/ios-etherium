fbd50b4763a0e1ae5cd309f021751c726c28a1ae
Troy Stribling <troy.stribling@gmail.com>
2014-04-28 19:31:16 -0700

    fixed typo in main build script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 ( cd ios-boost; ./boost.sh )
 ( cd ios-miniupnpc; ./miniupnpc.sh )
 ( cd ios-miniupnpc; ./miniupnpc.sh )
-( cd ios-cryptopp; ./cryptopp )
+( cd ios-cryptopp; ./cryptopp.sh )
 ( cd ios-gmp; ./gmp.sh )
 ( cd ios-leveldb; ./leveldb.sh )
 ( cd ios-ethereum; ./ethereum.sh )
