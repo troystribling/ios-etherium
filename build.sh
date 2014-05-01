c4b102c43a1542d31a3a6e184104f3f0c52d97c7
Troy Stribling <troy.stribling@gmail.com>
2014-05-01 08:33:03 -0700

    now enhanced

--- build.sh
+++ build.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
-( cd ios-boost; ./boost.sh )
-( cd ios-miniupnpc; ./miniupnpc.sh )
-( cd ios-miniupnpc; ./miniupnpc.sh )
-( cd ios-cryptopp; ./cryptopp.sh )
-( cd ios-gmp; ./gmp.sh )
-( cd ios-leveldb; ./leveldb.sh )
-( cd ios-secp256k1; ./secp256k1.sh )
-( cd ios-libethcore; ./ethcore.sh )
-( cd ios-ethereum; ./ethereum.sh )
+( cd ios-boost; \
+  ./boost.sh \
+  xcrun -sdk iphoneos lipo -info framework/boost.framework/Versions/A/boost )
+
+( cd ios-miniupnpc; \
+  ./miniupnpc.sh \
+  xcrun -sdk iphoneos lipo -info framework/miniupnpc.framework/Versions/A/miniupnpc )
+
+( cd ios-cryptopp; \
+  ./cryptopp.sh \
+  xcrun -sdk iphoneos lipo -info framework/cryptopp.framework/Versions/A/cryptopp )
+
+( cd ios-gmp; \
+  ./gmp.sh \
+  xcrun -sdk iphoneos lipo -info framework/gmp.framework/Versions/A/gmp )
+
+( cd ios-leveldb; \
+  ./leveldb.sh \
+  xcrun -sdk iphoneos lipo -info framework/leveldb.framework/Versions/A/leveldb )
+
+( cd ios-secp256k1; \
+ ./secp256k1.sh \
+ xcrun -sdk iphoneos lipo -info framework/secp256k1.framework/Versions/A/secp256k1 )
+
+( cd ios-libethcore; \
+  ./ethcore.sh \
+  xcrun -sdk iphoneos lipo -info framework/ethcore.framework/Versions/A/ethcore )
+
+( cd ios-ethereum; \
+ ./ethereum.sh \
+ xcrun -sdk iphoneos lipo -info framework/ethereum.framework/Versions/A/ethereum )
